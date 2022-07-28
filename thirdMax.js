f777fbc67ad5eb63d15bdb30551dd2f721c181dc
Mortron <cmsl4y3r@gmail.com>
2022-07-27 18:17:12 -0600

    fighting the 3rd max one

--- thirdMax.js
+++ thirdMax.js
@@ -2,37 +2,29 @@ var thirdMax = function(nums) {
     let i = 0;
     let max1 = 0;
     let max2 = 0;
-    let max3 = 0;
+    let max3 = Math.max(...nums);
     while(i < nums.length){
-        if(nums[i] > max3){
-            max3 = nums[i]
-            if(i > 0){
-                i--
-            }
-        }
+        console.log(max3, max2, max1, nums)
         if(nums[i] > max2 && nums[i] < max3){
             max2 = nums[i]
             if(i > 0){
-                i--
+                i = 0
             }
         }
         if(nums[i] > max1 && nums[i] < max2){
             max1 = nums[i]
-            if(i > 0){
-                i--
-            }
         }
-        i++
-        console.log(i, max3, max2, max1)
+        if(nums.length < 3){
+            return max3
+        }
+        if(max1 === 0){
+            return max3
+        } else {
+            return max1
+        }
     }
-    if(max1 === 0){
-        return max3
-    } else {
-        return max1
-    }
+}
 
-};
-
-console.log(thirdMax([1,2,2,5,3,5]))
+console.log(thirdMax([1,2, -2147483648]))
 
 thirdMax(2,2,3,1)
