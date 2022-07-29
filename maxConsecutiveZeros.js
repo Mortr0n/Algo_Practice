a117b97fd656016ce5e9670992c20e2eb7620c2a
Mortron <cmsl4y3r@gmail.com>
2022-07-28 18:50:06 -0600

    Rich Customer MultiDimensional array Algo

---
+++ maxConsecutiveZeros.js
@@ -0,0 +1,9 @@
+const findMaxConsecutiveZeros = (nums) => {
+    let longest = 0;
+    let zeros = 0;
+    let left = 0;
+    let right = 0;
+
+}
+
+console.log(findMaxConsecutiveZeros([1,0,0,1,1,0,1]))
